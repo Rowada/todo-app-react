903297eff94416609d516ebb3a15b7b333a69870
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-25 15:38:19 +0100

    build: add tailwind config for theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello World !!</h1>
+        <h1 className="text-3xl font-bold underline p-10 bg-background">
+          Hello World !!
+        </h1>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Josefin Sans", "sans-serif"],
+      },
+
+      colors: {
+        primary: "hsl(220, 98%, 61%)",
+        gradientValue1: "hsl(192, 100%, 67%)",
+        gradientValue2: "hsl(280, 87%, 65%)",
+        body: "var(--body)",
+        background: "var(--background)",
+        accent: "var(--accent)",
+        secondary: "var(--secondary)",
+        text: "var(--text)",
+        other: "var(--other)",
+        hover: "var(--hover)",
+      },
+    },
   },
   plugins: [],
 };
