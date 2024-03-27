e51fbd793dbe921a265df46c519324db5c76c3ad
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-27 11:19:49 +0100

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import useStore from "./store";
 import ThemeProvider from "./context/ThemeProvider";
-import ToggleTheme from "./components/toggleTheme/ToggleTheme";
+import { Header } from "./components/Header";
+import { TodoList } from "./components/todos/TodoList";
 
 function App() {
   const theme = useStore((state) => state.theme);
   return (
     <ThemeProvider value={theme}>
-      <>
-        <div>
-          <h1 className="text-3xl font-bold underline p-10 bg-background">
-            Hello World !!
-          </h1>
-        </div>
-
-        <ToggleTheme />
-      </>
+      <main>
+        <Header />
+        <TodoList />
+      </main>
     </ThemeProvider>
   );
 }
