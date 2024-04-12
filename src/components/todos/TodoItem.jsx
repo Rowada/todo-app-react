f9444a43b0d2c3488e66f6d8430992e3c73df2b0
Josue <96113041+Rowada@users.noreply.github.com>
2024-04-12 15:52:58 +0200

    fix: add label for checkbox

--- src/components/todos/TodoItem.jsx
+++ src/components/todos/TodoItem.jsx
@@ -10,18 +10,20 @@ export const TodoItem = ({ todo, id }) => {
   return (
     <>
       <li className="inset-y-0 flex items-center">
-        <input
-          checked={todo.isCompleted}
-          onChange={() => toggleTodo(id)}
-          style={{ "--image-url": `url(${checkIcon})` }}
-          type="checkbox"
-          id="checkbox"
-          className="peer relative transition-all rounded-full w-5 h-5 appearance-none focus:outline-none border border-secondary checked:after:block after:hidden checked:bg-gradient-to-r from-gradientValue1 to-gradientValue2 checked:border-none hover:border-primary shrink-0 after:content-[''] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-no-repeat after:bg-center after:bg-[image:var(--image-url)] cursor-pointer"
-        />
+        <label htmlFor="checkbox" className="flex items-center">
+          <input
+            checked={todo.isCompleted}
+            onChange={() => toggleTodo(id)}
+            style={{ "--image-url": `url(${checkIcon})` }}
+            type="checkbox"
+            id="checkbox"
+            className="peer relative transition-all rounded-full w-5 h-5 appearance-none focus:outline-none border border-secondary checked:after:block after:hidden checked:bg-gradient-to-r from-gradientValue1 to-gradientValue2 checked:border-none hover:border-primary shrink-0 after:content-[''] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-no-repeat after:bg-center after:bg-[image:var(--image-url)] cursor-pointer"
+          />
 
-        <p className="ps-4 transition-colors peer-checked:line-through peer-checked:text-secondary text-xs md:text-base">
-          {todo.text}
-        </p>
+          <p className="ps-4 transition-colors peer-checked:line-through peer-checked:text-secondary text-xs md:text-base">
+            {todo.text}
+          </p>
+        </label>
       </li>
 
       <button className="pe-4" onClick={() => removeTodo(id)}>
